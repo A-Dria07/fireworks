e94efe5181488ce796a025293c23454bf166f4ec
fparc <fparc@DESKTOP-RAIV40F>
2024-10-06 12:15:03 +0200

    Added icons

--- src/App.js
+++ src/App.js
@@ -10,13 +10,49 @@ const App = () => {
   const [power, setPower] = useState(12); // Default power value
   const [metal, setMetal] = useState(12); // Defaultmetal value
   const [count, setCount] = useState(0);
- 
+  const [menuOpen, setMenuOpen] = useState(false); // Control menu visibility
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <div style={{ flex: 1 }}>
         <ParticleCanvas  colors={selectedColors} power={power} metal = {metal} setCount={setCount} /> {/* Pass power value */}
       </div>
       <div style={{ padding: '20px', position: 'fixed', right: '10px', top: '10px' }}>
+      {/* Hamburger Icon */}
+      <div style={{ position: 'fixed', top: '10px', right: '10px', zIndex: 10 }}>
+        <button 
+          onClick={toggleMenu} 
+          style={{
+            backgroundColor: 'transparent',
+            border: 'none',
+            cursor: 'pointer',
+            fontSize: '30px',
+            color: 'white',
+          }}
+        >
+          &#9776; {/* Hamburger icon */}
+        </button>
+      </div>
+
+      {/* Side Menu (Color Picker) */}
+      <div
+        style={{
+          position: 'fixed',
+          right: menuOpen ? '0' : '-400px', // Slide in/out
+          top: '0',
+          height: '100%',
+          width: '300px',
+          backgroundColor: '#222',
+          transition: 'right 0.3s ease',
+          padding: '20px',
+          boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
+          zIndex: 9,
+        }}
+      >
+      
+
+        {/* Color Picker Component */}
         <ColorPicker 
           selectedColors={selectedColors} 
           setSelectedColors={setSelectedColors}
@@ -26,12 +62,14 @@ const App = () => {
           setMetal={setMetal} // Pass setPower to ColorPicker
         />
 
+      </div>
+
     <div style={{
         position: 'fixed',
         top: '10px',
-        left: '100px', // Adjust the position as needed
+        left: '20px', // Adjust the position as needed
         color: 'white',
-        fontSize: '24px',
+        fontSize: '100%',
       }}>
         CO2 emissions: {count.toFixed(5)} Kg
       </div>
